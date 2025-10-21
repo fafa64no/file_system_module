5dd2f664ad15f8b87b9964ce870d9a1051a09ac4
Sébastien Nouira Huet <148809822+fafa64no@users.noreply.github.com>
2025-10-21 20:38:04 +0200

    [UPDATE] Part 1 : Added display of blocks

--- file_mapping.c
+++ file_mapping.c
@@ -15,6 +15,11 @@
 
 #define SYSTEM_CALL_ERROR (-1)
 #define MAX_INODE_NUMBER (TOSFS_BLOCK_SIZE / sizeof(struct tosfs_inode))
+#define MAX_INODE_ENTRY_NUMBER (TOSFS_BLOCK_SIZE / sizeof(struct tosfs_inode))
+#define MAX_NB_DATA_BLOCKS (29)
+#define PRINT_ROW_SIZE (64)
+#define POS_OF_BLOCK_3_IN_DATA_BLOCKS (0)
+#define POS_OF_BLOCK_4_IN_DATA_BLOCKS (1)
 
 
 struct mapped_file_struct {
@@ -24,7 +29,8 @@ struct mapped_file_struct {
 
     struct tosfs_superblock* superblock;
     struct tosfs_inode* inodes;
-    struct tosfs_dentry* entries;
+    struct tosfs_dentry* root_block;
+    struct tosfs_dentry* data_blocks;
 };
 
 
@@ -82,7 +88,10 @@ void read_mapped_file_as_tosfs_file(struct mapped_file_struct* mapped_file) {
     mapped_file->inodes = (struct tosfs_inode*) tmp_ptr;
 
     tmp_ptr += TOSFS_BLOCK_SIZE;
-    mapped_file->entries = (struct tosfs_dentry*) tmp_ptr;
+    mapped_file->root_block = (struct tosfs_dentry*) tmp_ptr;
+
+    tmp_ptr += TOSFS_BLOCK_SIZE;
+    mapped_file->data_blocks = (struct tosfs_dentry*) tmp_ptr;
 }
 
 
@@ -115,22 +124,101 @@ void disp_structure_tosfs_inode(const struct mapped_file_struct* mapped_file) {
     }
 }
 
+void disp_structure_block_tosfs_dentry(const struct tosfs_dentry* disk_entry) {
+    for (unsigned int entry_number = 0; entry_number < MAX_INODE_ENTRY_NUMBER; entry_number++) {
+        if (disk_entry->inode == 0) {
+            break;
+        }
+
+        printf(
+            "Reading disk entry nb %u:"
+            "\n\tinode: %d"
+            "\n\tname: %s"
+            "\n",
+            entry_number,
+            disk_entry->inode,
+            disk_entry->name
+        );
+        disk_entry++;
+    }
+}
+
+void disp_data_block(const void* data_block) {
+    printf(
+        "\tdata block as str:\n%s\n",
+        (const char*)data_block
+    );
+    printf(
+        "\tdata block as binary:"
+    );
+
+    for (unsigned int i = 0; i < TOSFS_BLOCK_SIZE; i++) {
+        if (i % PRINT_ROW_SIZE == 0) {
+            printf("\n");
+        }
+        const char byte_to_print = ((const char*)data_block)[i];
+        printf(PRINTF_BINARY_PATTERN_INT8, PRINTF_BYTE_TO_BINARY_INT8(byte_to_print));
+    }
+}
+
+void disp_structure_all_data_blocks(const struct mapped_file_struct* mapped_file) {
+    for (unsigned int block_number = 0; block_number < MAX_NB_DATA_BLOCKS; block_number++) {
+        printf(
+            "Data block nb %u:\n"
+            "%s",
+            block_number,
+            (const char*)&mapped_file->data_blocks[block_number]
+        );
+    }
+}
+
+void disp_structure_tosfs_dentry(const struct mapped_file_struct* mapped_file) {
+    const unsigned int root_inode_number = mapped_file->superblock->root_inode;
+    const unsigned int last_inode_number = root_inode_number + mapped_file->superblock->inodes;
+
+    for (unsigned int i = root_inode_number; i < last_inode_number; i++) {
+        const struct tosfs_inode* inode = &mapped_file->inodes[i];
+        const unsigned int block_number = inode->block_no;
+
+        const struct tosfs_dentry* disk_entry = &mapped_file->root_block[block_number];
+        printf(
+            "Reading disk entry nb %u:"
+            "\n\tinode: %d"
+            "\n\tname: %s"
+            "\n",
+            block_number,
+            disk_entry->inode,
+            disk_entry->name
+        );
+    }
+}
+
+
 void disp_structures_tosfs() {
     struct mapped_file_struct* mapped_file = map_example_file();
     read_mapped_file_as_tosfs_file(mapped_file);
 
-    printf("\n----------------------------------------------\n");
+    printf("\n----------------------------------------------\nSuperblock is:\n");
     disp_structure_tosfs_superblock(mapped_file);
-    printf("\n");
+    printf("\n----------------------------------------------\nInode map is:\n");
     disp_structure_tosfs_inode(mapped_file);
-    printf("----------------------------------------------\n");
+    printf("\n----------------------------------------------\nRoot block is:\n");
+    disp_structure_block_tosfs_dentry(mapped_file->root_block);
+    printf("\n----------------------------------------------\nData block 3 is:\n");
+    disp_data_block(&mapped_file->data_blocks[POS_OF_BLOCK_3_IN_DATA_BLOCKS]);
+    printf("\n----------------------------------------------\nData block 4 is:\n");
+    disp_data_block(&mapped_file->data_blocks[POS_OF_BLOCK_4_IN_DATA_BLOCKS]);
+    printf("\n----------------------------------------------\n");
 
     close_mapped_file(mapped_file);
 }
 
 
 int main(int argc, char *argv[]) {
-    printf("\ntosfs can support %lu inodes\n", MAX_INODE_NUMBER);
+    printf(
+        "\nINFO: tosfs can support %lu inodes and %lu inode entries\n",
+        MAX_INODE_NUMBER, MAX_INODE_ENTRY_NUMBER
+    );
 
     disp_structures_tosfs();
 
